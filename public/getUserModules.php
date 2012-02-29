ed542e5273a00c52e5ec05931b3c067f1fece7b6
Andrew Rivett <veggiefrog@gmail.com>
2012-02-29 07:40:34 -0500

    Added atdo module, and transfer functionality from calendars and tasks to atdo

--- public/getUserModules.php
+++ public/getUserModules.php
@@ -53,14 +53,15 @@ function ciniki_businesses_getUserModules($ciniki) {
 	//
 	if( $rsp['stat'] == 'ok' ) {
 		foreach($rsp['modules'] as $i => $module) {
-			if( $module['module']['name'] == 'ciniki.tasks' ) {
-				$strsql = "SELECT 'numtasks', COUNT(ciniki_tasks.id) "
-					. "FROM ciniki_tasks, ciniki_task_users "
+			if( $module['module']['name'] == 'ciniki.atdo' ) {
+				$strsql = "SELECT 'numtasks', COUNT(ciniki_atdos.id) "
+					. "FROM ciniki_atdos, ciniki_atdo_users "
 					. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
-					. "AND ciniki_tasks.id = ciniki_task_users.task_id "
-					. "AND ciniki_task_users.user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
-					. "AND ciniki_tasks.status = 1 "
-					. "AND (ciniki_task_users.perms&0x04) = 0x04 "
+					. "AND ciniki_atdos.type = 2 "	// Tasks
+					. "AND ciniki_atdos.id = ciniki_atdo_users.atdo_id "
+					. "AND ciniki_atdo_users.user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
+					. "AND ciniki_atdos.status = 1 "
+					. "AND (ciniki_atdo_users.perms&0x04) = 0x04 "
 					. "";
 				ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbCount');
 				$rc = ciniki_core_dbCount($ciniki, $strsql, 'tasks', 'tasks');
