4b8310816368697096a8f5c0f391a639c3bdff38
Andrew Rivett <veggiefrog@gmail.com>
2012-05-15 11:32:42 -0400

    Added featured business buttons and about information to master index page

--- web/featured.php
+++ web/featured.php
@@ -18,9 +18,13 @@ function ciniki_businesses_web_featured($ciniki, $settings) {
 	// Exclude the master business
 	//
 	$strsql = "SELECT ciniki_businesses.id, ciniki_businesses.name, ciniki_businesses.sitename, ciniki_business_domains.domain "
-		. "FROM ciniki_businesses "
+		. "FROM ciniki_business_modules, ciniki_businesses "
 		. "LEFT JOIN ciniki_business_domains ON (ciniki_businesses.id = ciniki_business_domains.business_id AND (ciniki_business_domains.flags&0x01) = 0x01 ) "
 		. "WHERE ciniki_businesses.id <> '" . ciniki_core_dbQuote($ciniki, $ciniki['config']['core']['master_business_id']) . "' "
+		. "AND ciniki_businesses.id = ciniki_business_modules.business_id "
+		. "AND ciniki_business_modules.package = 'ciniki' "
+		. "AND ciniki_business_modules.module = 'web' "
+		. "AND ciniki_business_modules.status = 1 "
 		. "ORDER BY ciniki_businesses.name "
 		. "";
 
