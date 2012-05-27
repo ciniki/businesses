ddac0ac2246fbc30606f6704a4dc9837ac67a7d6
Andrew Rivett <veggiefrog@gmail.com>
2012-05-27 08:32:42 -0400

    Added billing report to admin

--- public/get.php
+++ public/get.php
@@ -48,14 +48,22 @@ function ciniki_businesses_get($ciniki) {
 	require_once($ciniki['config']['core']['modules_dir'] . '/users/private/datetimeFormat.php');
 	$date_format = ciniki_users_datetimeFormat($ciniki);
 
+	require_once($ciniki['config']['core']['modules_dir'] . '/users/private/timezoneOffset.php');
+	$utc_offset = ciniki_users_timezoneOffset($ciniki);
+
 	//
 	// Get all the information form ciniki_users table
 	//
-	$strsql = "SELECT id, uuid, name, status, "
-		. "DATE_FORMAT(date_added, '" . ciniki_core_dbQuote($ciniki, $date_format) . "') AS date_added, "
-		. "DATE_FORMAT(last_updated, '" . ciniki_core_dbQuote($ciniki, $date_format) . "') AS last_updated "
+	$strsql = "SELECT ciniki_businesses.id, ciniki_businesses.uuid, ciniki_businesses.name, ciniki_businesses.status AS business_status, "
+		. "DATE_FORMAT(ciniki_businesses.date_added, '" . ciniki_core_dbQuote($ciniki, $date_format) . "') AS date_added, "
+		. "DATE_FORMAT(ciniki_businesses.last_updated, '" . ciniki_core_dbQuote($ciniki, $date_format) . "') AS last_updated, "
+		. "ciniki_business_subscriptions.status AS subscription_status, signup_date, trial_days, "
+		. "currency, monthly, paypal_subscr_id, paypal_payer_email, paypal_payer_id, paypal_amount, "
+		. "IF(last_payment_date='0000-00-00', '', DATE_FORMAT(CONVERT_TZ(ciniki_business_subscriptions.date_added, '+00:00', '" . ciniki_core_dbQuote($ciniki, $utc_offset) . "'), '%b %e, %Y %l:%i %p')) AS last_payment_date, "
+		. "trial_days - FLOOR((UNIX_TIMESTAMP(UTC_TIMESTAMP())-UNIX_TIMESTAMP(ciniki_business_subscriptions.signup_date))/86400) AS trial_remaining "
 		. "FROM ciniki_businesses "
-		. "WHERE id = '" . ciniki_core_dbQuote($ciniki, $args['id']) . "' "
+		. "LEFT JOIN ciniki_business_subscriptions ON (ciniki_businesses.id = ciniki_business_subscriptions.business_id) "
+		. "WHERE ciniki_businesses.id = '" . ciniki_core_dbQuote($ciniki, $args['id']) . "' "
 		. "";
 	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.businesses', 'business');
 	if( $rc['stat'] != 'ok' ) {
@@ -65,6 +73,34 @@ function ciniki_businesses_get($ciniki) {
 		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'513', 'msg'=>'Unable to find business'));
 	}
 	$business = $rc['business'];
+
+	if( !isset($business['monthly']) ) {
+		$business['monthly'] = 0;
+	}
+	if( !isset($business['trial_remaining']) ) {
+		$business['trial_remaining'] = 0;
+	}
+
+	//
+	// Map subscription status
+	//
+	if( !isset($business['subscription_status_text']) ) {
+		$business['subscription_status_text'] = 'None';
+	} elseif( $business['subscription_status'] == 0 ) {
+		$business['subscription_status_text'] = 'Unknown';
+	} elseif( $business['subscription_status'] == 1 ) {
+		$business['subscription_status_text'] = 'Update required';
+	} elseif( $business['subscription_status'] == 2 ) {
+		$business['subscription_status_text'] = 'Payment information required';
+	} elseif( $business['subscription_status'] == 10 ) {
+		$business['subscription_status_text'] = 'Active';
+	} elseif( $business['subscription_status'] == 50 ) {
+		$business['subscription_status_text'] = 'Suspended';
+	} elseif( $business['subscription_status'] == 60 ) {
+		$business['subscription_status_text'] = 'Cancelled';
+	} elseif( $business['subscription_status'] == 61 ) {
+		$business['subscription_status_text'] = 'Pending Cancel';
+	}
 
 	//
 	// Get all the businesses the user is a part of
