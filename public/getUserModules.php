efa5c93c7e613c81ed86a91c9bab9901847b827e
Andrew Rivett <veggiefrog@gmail.com>
2012-09-14 13:28:03 -0400

    Updated getUserModules to include project stats

--- public/getUserModules.php
+++ public/getUserModules.php
@@ -20,7 +20,7 @@ function ciniki_businesses_getUserModules($ciniki) {
 	//
 	// Find all the required and optional arguments
 	//
-	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/prepareArgs.php');
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'prepareArgs');
 	$rc = ciniki_core_prepareArgs($ciniki, 'no', array(
 		'business_id'=>array('required'=>'yes', 'blank'=>'no', 'errmsg'=>'No business specified'), 
 		));
@@ -32,7 +32,7 @@ function ciniki_businesses_getUserModules($ciniki) {
 	//
 	// Check access to business_id as owner, or sys admin
 	//
-	require_once($ciniki['config']['core']['modules_dir'] . '/businesses/private/checkAccess.php');
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'businesses', 'private', 'checkAccess');
 	$rc = ciniki_businesses_checkAccess($ciniki, 0, 'ciniki.businesses.getUserModules');
 	if( $rc['stat'] != 'ok' ) {
 		return $rc;
@@ -54,36 +54,41 @@ function ciniki_businesses_getUserModules($ciniki) {
 	if( $rsp['stat'] == 'ok' ) {
 		foreach($rsp['modules'] as $i => $module) {
 			if( $module['module']['name'] == 'ciniki.atdo' ) {
-				$strsql = "SELECT 'numtasks', COUNT(ciniki_atdos.id) "
-					. "FROM ciniki_atdos, ciniki_atdo_users "
-					. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
-					. "AND ciniki_atdos.type = 2 "	// Tasks
-					. "AND ciniki_atdos.id = ciniki_atdo_users.atdo_id "
-					. "AND ciniki_atdo_users.user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
-					. "AND ciniki_atdos.status = 1 "
-					. "AND (ciniki_atdo_users.perms&0x04) = 0x04 "
-					. "";
-				ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbCount');
-				$rc = ciniki_core_dbCount($ciniki, $strsql, 'ciniki.atdo', 'atdo');
-				if( $rc['stat'] != 'ok' ) {
-					return $rc;
-				}
-				$rsp['modules'][$i]['module']['task_count'] = $rc['atdo']['numtasks'];
+//				$strsql = "SELECT 'numtasks', COUNT(ciniki_atdos.id) "
+//					. "FROM ciniki_atdos, ciniki_atdo_users "
+//					. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+//					. "AND ciniki_atdos.type = 2 "	// Tasks
+//					. "AND ciniki_atdos.id = ciniki_atdo_users.atdo_id "
+//					. "AND ciniki_atdo_users.user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
+//					. "AND ciniki_atdos.status = 1 "
+//					. "AND (ciniki_atdo_users.perms&0x04) = 0x04 "
+//					. "";
+//				ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbCount');
+//				$rc = ciniki_core_dbCount($ciniki, $strsql, 'ciniki.atdo', 'atdo');
+//				if( $rc['stat'] != 'ok' ) {
+//					return $rc;
+//				}
+//				$rsp['modules'][$i]['module']['task_count'] = $rc['atdo']['numtasks'];
 				$strsql = "SELECT type, COUNT(ciniki_atdos.id) AS num_items "
 					. "FROM ciniki_atdos, ciniki_atdo_users "
 					. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
-					. "AND (ciniki_atdos.type = 6 OR ciniki_atdos.type = 5 )"	// Messages or Notes
+					. "AND (ciniki_atdos.type = 2 OR ciniki_atdos.type = 6 OR ciniki_atdos.type = 5 )"	// Messages or Notes
 					. "AND ciniki_atdos.id = ciniki_atdo_users.atdo_id "
 					. "AND ciniki_atdo_users.user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
 					. "AND ciniki_atdos.status = 1 "
 					. "AND (ciniki_atdo_users.perms&0x04) = 0x04 "
-					. "AND (ciniki_atdo_users.perms&0x08) = 0 "
+//					. "AND (ciniki_atdo_users.perms&0x08) = 0 "
 					. "GROUP BY type "
 					. "";
 				ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashIDQuery');
 				$rc = ciniki_core_dbHashIDQuery($ciniki, $strsql, 'ciniki.atdo', 'atdo', 'type');
 				if( $rc['stat'] != 'ok' ) {
 					return $rc;
+				}
+				if( isset($rc['atdo']['2']['num_items']) ) {
+					$rsp['modules'][$i]['module']['task_count'] = 0 + $rc['atdo']['2']['num_items'];
+				} else {
+					$rsp['modules'][$i]['module']['task_count'] = 0;
 				}
 				if( isset($rc['atdo']['6']['num_items']) ) {
 					$rsp['modules'][$i]['module']['message_count'] = 0 + $rc['atdo']['6']['num_items'];
