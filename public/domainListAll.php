3bdade94c3cb40857beb2ac63d09aa0f5e7ce449
Andrew Rivett <veggiefrog@gmail.com>
2013-03-27 16:08:14 -0400

    Added ability to list all domains for all businesses

--- public/domainListAll.php
+++ public/domainListAll.php
@@ -2,7 +2,6 @@
 //
 // Description
 // -----------
-// This function will lookup the client domain in the database, and return the business id.
 //
 // Arguments
 // ---------
@@ -10,13 +9,13 @@
 // Returns
 // -------
 //
-function ciniki_businesses_domainExpiries($ciniki) {
+function ciniki_businesses_domainListAll($ciniki) {
 	//
 	// Find all the required and optional arguments
 	//
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'prepareArgs');
 	$rc = ciniki_core_prepareArgs($ciniki, 'no', array(
-		'days'=>array('required'=>'no', 'blank'=>'no', 'default'=>'90', 'name'=>'Days'),
+		'limit'=>array('required'=>'no', 'blank'=>'no', 'default'=>'0', 'name'=>'Limit'),
 		));
 	if( $rc['stat'] != 'ok' ) {
 		return $rc;
@@ -27,7 +26,7 @@ function ciniki_businesses_domainExpiries($ciniki) {
 	// Check access to business_id as owner
 	//
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'businesses', 'private', 'checkAccess');
-	$ac = ciniki_businesses_checkAccess($ciniki, 0, 'ciniki.businesses.domainExpiries');
+	$ac = ciniki_businesses_checkAccess($ciniki, 0, 'ciniki.businesses.domainListAll');
 	if( $ac['stat'] != 'ok' ) {
 		return $ac;
 	}
@@ -45,27 +44,25 @@ function ciniki_businesses_domainExpiries($ciniki) {
 		. "ciniki_business_domains.domain, "
 		. "ciniki_business_domains.flags, "
 		. "ciniki_business_domains.status, "
-		. "ciniki_business_domains.status AS status_text, "
 		. "IF((ciniki_business_domains.flags&0x01)=0x01, 'yes', 'no') AS isprimary, "
 		. "IFNULL(DATE_FORMAT(ciniki_business_domains.expiry_date, '" . ciniki_core_dbQuote($ciniki, $date_format) . "'), 'expiry unknown') AS expiry_date, "
-		. "IFNULL(DATEDIFF(ciniki_business_domains.expiry_date, UTC_TIMESTAMP()), -999) AS expire_in_days "
+		. "DATEDIFF(ciniki_business_domains.expiry_date, UTC_TIMESTAMP()) AS expire_in_days "
 		. "FROM ciniki_business_domains "
 		. "LEFT JOIN ciniki_businesses ON (ciniki_business_domains.business_id = ciniki_businesses.id) "
-		. "WHERE DATEDIFF(ciniki_business_domains.expiry_date,UTC_TIMESTAMP()) < '" . $args['days'] . "' "
-		. "OR ciniki_business_domains.expiry_date = '0000-00-00' "
-		. "ORDER BY expire_in_days ASC "
+		. "ORDER BY ciniki_businesses.name "
 		. "";
+	if( isset($args['limit']) && $args['limit'] > 0 ) {
+		$strsql .= "LIMIT '" . ciniki_core_dbQuote($ciniki, $args['limit']) . "' ";
+	}
 
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryTree');
 	$rc = ciniki_core_dbHashQueryTree($ciniki, $strsql, 'ciniki.businesses', array(
 		array('container'=>'domains', 'fname'=>'id', 'name'=>'domain',
 			'fields'=>array('id', 'business_id', 'business_name', 'business_status',
-				'domain', 'flags', 'status', 'status_text', 'isprimary', 
+				'domain', 'flags', 'status', 'isprimary', 
 				'expiry_date', 'expire_in_days'),
-			'maps'=>array(
-				'business_status'=>array('1'=>'Active', '10'=>'Suspended', '60'=>'Deleted'),
-				'status_text'=>array('1'=>'Active', '20'=>'Expired', '50'=>'Suspended', '60'=>'Deleted'),
-			)),
+			'maps'=>array('business_status'=>array('1'=>'Active', '10'=>'Suspended', '60'=>'Deleted')),
+			),
 		));
 
 	return $rc;
