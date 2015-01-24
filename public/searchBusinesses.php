4c681f3205cc1fe1e8acad2ee3abe1ccccfd41b8
Andrew Rivett <veggiefrog@gmail.com>
2015-01-24 17:27:43 -0500

    Added ability to search in main menu for admin

---
+++ public/searchBusinesses.php
@@ -0,0 +1,67 @@
+<?php
+//
+// Description
+// -----------
+// This method will search a field for the search string provided.
+//
+// Arguments
+// ---------
+// api_key:
+// auth_token:
+// start_needle:	The search string to search the field for.
+//
+// limit:			(optional) Limit the number of results to be returned. 
+//					If the limit is not specified, the default is 25.
+// 
+// Returns
+// -------
+//
+function ciniki_businesses_searchBusinesses($ciniki) {
+    //  
+    // Find all the required and optional arguments
+    //  
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'prepareArgs');
+    $rc = ciniki_core_prepareArgs($ciniki, 'no', array(
+        'start_needle'=>array('required'=>'yes', 'blank'=>'yes', 'name'=>'Search'), 
+        'limit'=>array('required'=>'no', 'blank'=>'no', 'name'=>'Limit'), 
+        )); 
+    if( $rc['stat'] != 'ok' ) { 
+        return $rc;
+    }   
+    $args = $rc['args'];
+    
+    //  
+    // Make sure this module is activated, and
+    // check permission to run this function for this business
+    //  
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'businesses', 'private', 'checkAccess');
+    $rc = ciniki_businesses_checkAccess($ciniki, 0, 'ciniki.businesses.searchBusinesses'); 
+    if( $rc['stat'] != 'ok' ) { 
+        return $rc;
+    }   
+
+	//
+	// Search for categories
+	//
+	$strsql = "SELECT id, name "
+		. "FROM ciniki_businesses "
+		. "WHERE (name like '" . ciniki_core_dbQuote($ciniki, $args['start_needle']) . "%' "
+			. "OR name like '% " . ciniki_core_dbQuote($ciniki, $args['start_needle']) . "%' "
+			. ") "
+		. "AND name <> '' "
+		. "ORDER BY name COLLATE latin1_general_cs "
+		. "";
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryTree');
+	$rc = ciniki_core_dbHashQueryTree($ciniki, $strsql, 'ciniki.businesses', array(
+		array('container'=>'businesses', 'fname'=>'name', 'name'=>'business', 
+			'fields'=>array('id', 'name')),
+		));
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+	if( !isset($rc['businesses']) || !is_array($rc['businesses']) ) {
+		return array('stat'=>'ok', 'businesses'=>array());
+	}
+	return array('stat'=>'ok', 'businesses'=>$rc['businesses']);
+}
+?>
