a8fad2eaea91acd1d5c898aed7d2ca2716c85121
Andrew Rivett <veggiefrog@gmail.com>
2017-11-19 14:39:35 -0500

    Added script to auto check domain expirations

--- private/objects.php
+++ private/objects.php
@@ -12,6 +12,23 @@
 function ciniki_businesses_objects($ciniki) {
     
     $objects = array();
+
+    $objects['domain'] = array(
+        'name' => 'Domain Name',
+        'sync' => 'yes',
+        'o_name' => 'domain',
+        'o_container' => 'domains',
+        'table' => 'ciniki_business_domains',
+        'fields' => array(
+            'domain' => array('name'=>'Name'),
+            'flags' => array('name'=>'Options', 'default'=>'0'),
+            'status' => array('name'=>'Status', 'default'=>'1'),
+            'root_id,' => array('name'=>'', 'default'=>''),
+            'expiry_date' => array('name'=>'Expiration Date', 'default'=>''),
+            'managed_by' => array('name'=>'Managed By', 'default'=>''),
+            ),
+        'history_table' => 'ciniki_business_history',
+        );
     $objects['report'] = array(
         'name'=>'Reports',
         'sync'=>'yes',
